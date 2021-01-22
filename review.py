da8e26503e30ac31a0443e77df840cc2a040248c
linpengwei <schopfer1993@gmail.com>
2021-01-22 17:29:02 +0800

    make navbar with flask-bootrap

--- review.py
+++ review.py
@@ -1,3 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
