355c6bdbe5da289b3b96c759fcdbbed3f49b5345
linpengwei <schopfer1993@gmail.com>
2021-01-23 14:36:50 +0800

    add a form to upload photo

--- review.py
+++ review.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+class FaceForm(FlaskForm):
+    face_pic = FileField('YOUR FACE PHOTO', validators=[DataRequired()])
+    submit = SubmitField('Submit!')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    face_file = None
+    form = FaceForm()
+    if form.validate_on_submit():
+        face_file = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, face_file=face_file)
 
 @app.errorhandler(404)
 def page_not_found(e):
