919b62a6f72290bfc426ac9df8254172b1432b96
linpengwei <schopfer1993@gmail.com>
2021-01-23 16:20:22 +0800

    Update upload image function, and it can work

--- review.py
+++ review.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 from wtforms.validators import DataRequired
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/uploads')
 
-class FaceForm(FlaskForm):
-    face_pic = FileField('YOUR FACE PHOTO', validators=[DataRequired()])
-    submit = SubmitField('Submit!')
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)
+
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[FileAllowed(photos, 'Image only!'), FileRequired('File was empty!')])
+    submit = SubmitField('Upload!')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    face_file = None
-    form = FaceForm()
+    file_url = None
+    form = UploadForm()
     if form.validate_on_submit():
-        face_file = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, face_file=face_file)
+        filename = photos.save(form.photo.data)
+        session['file_url'] = file_url = photos.url(filename)
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, file_url=session.get('file_url'))
 
 @app.errorhandler(404)
 def page_not_found(e):
